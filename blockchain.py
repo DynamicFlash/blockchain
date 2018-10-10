a66a2fca6a08bd54c1d3322a5030e2c2cbb52174
DynamicFlash <aldrin.a.fernandes.com>
2018-10-10 13:11:16 +0530

    building of blockchain

--- blockchain.py
+++ blockchain.py
@@ -41,3 +41,18 @@ class Blockchain:
         encoded_block = json.dumps(block, sorted_keys = True).encode()
         return hashing.sha256(encoded_block).hexdigest()
     
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof =previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashing.sha256(str(proof**3 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != "0000":
+                return False
+            previous_block = block
+            block_index += 1
+        return True
