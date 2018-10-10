f60ec3a863ecad242a3b4c6dc73419f29caa8408
DynamicFlash <aldrin.a.fernandes.com>
2018-10-10 12:35:18 +0530

    check_proof

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,39 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct  3 20:19:52 2018
+#creating the blockchain
 
-@author: Aldrin Fernandes
-"""
+import datetime
+import hashlib as hashing
+import json
+from flask import Flask, jsonify
 
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+    
+    def create_block(self, proof, previous_hash):
+        block = {'index' : len(self.chain)+1, 
+                 'timestap': str(datetime.datetime.now()),
+                 'proof' : proof,
+                 'previous_hash' : previous_hash
+                 }
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
+        
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        
+        while check_proof is False: 
+            hash_operation = hashing.sha256(str(new_proof**3 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] is "0000":
+                check_proof = True
+            else:
+                new_proof+=1
+                
+        return new_proof
+    
